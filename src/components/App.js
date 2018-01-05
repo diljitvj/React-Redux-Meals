a5a6bcf67c3fd613a2721b8fddf258f258947631
Diljit VJ <diljitvj@gmail.com>
2018-01-05 21:29:52 +0530

    Add connect and mapStateToProps to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class App extends Component {
  
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <h1>Hello World</h1>
@@ -11,5 +12,15 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+function mapStateToProps(state){
+  // console.log(state);
+  const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+  let calender = []
+  calender = days.map((day)=>({
+    day: day,
+    meals : state[day]
+  }))
+  // console.log(temp);
+  return {calender : calender};
+}
+export default connect(mapStateToProps)(App);
